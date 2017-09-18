814eb692a3155e160b270ea0056f3f448228f14e
zhen fu <fuzhen327@gmail.com>
2017-09-18 09:58:06 -0700

    my first commit

---
+++ gruoped_Bar.R
@@ -0,0 +1,20 @@
+list.files()
+df <- read.csv("farm_sp_richness_example.txt", header = T, sep = "\t")
+head(df)
+library(plyr)
+library(ggplot2)
+split_df <-ddply(df, c("Farm_management", "Geographic_location"), 
+                 summarise, N=length(species_richness), mean=mean(species_richness), 
+                 sd=sd(species_richness),
+                  se=sd/sqrt(N))
+
+ggplot(split_df, aes(fill=Farm_management, y=mean, x=Geographic_location)) + 
+  geom_bar(position=position_dodge(0.9), stat="identity") +
+  geom_errorbar(aes(ymax = split_df$mean + split_df$se,
+                    ymin = split_df$mean - split_df$se),
+                    width = 0.25,
+                    position = position_dodge(0.9)) +
+    xlab("Farm Management") +
+    ylab("Species richness") +
+    scale_fill_manual(name = "Farm_managemen",
+                      values = c("goldenrod2", "olivedrab4"))
